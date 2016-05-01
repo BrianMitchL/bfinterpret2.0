f60e62c6f14ee974e95ef192d44cfd78aec0c007
Brian Mitchell <bman4789@gmail.com>
2016-05-01 18:00:34 -0500

    Add comments for new and old CS->KEY notation. Add *.o and *.exe to .gitignore. Exclude crc/ from build.

--- clock.c
+++ clock.c
@@ -7,21 +7,36 @@
 
 #include "clock.h"
 
+// Use this for the older version of CCStudio
+//void SetClockFrequency(void)
+//{
+//	CSKEY = 0x695A;
+//	CSCTL0 = 0x00030000;
+//	CSCTL1 = 0x00000233;
+//	CSCLKEN = 0x0000800F;
+//	CSKEY = 0xA596;
+//}
+//
+//unsigned int ReadClockFrequency(void)
+//{
+//	unsigned int qret=CSCTL0;
+//	return qret;
+//}
+
+// Use this for the newer version of CCStudio
 void SetClockFrequency(void)
 {
-	CSKEY = 0x695A;
-	CSCTL0 = 0x00030000;
-	CSCTL1 = 0x00000233;
-	CSCLKEN = 0x0000800F;
-	CSKEY = 0xA596;
+	CS->KEY = 0x695A;
+	CS->CTL0 = 0x00030000;
+	CS->CTL1 = 0x00000233;
+	CS->CLKEN = 0x0000800F;
+	CS->KEY = 0xA596;
 }
 
 unsigned int ReadClockFrequency(void)
 {
-	unsigned int qret=CSCTL0;
+	unsigned int qret=CS->CTL0;
 	return qret;
 }
-
-
 
 
